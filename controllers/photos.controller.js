752169f75faff16c0b3293e0298f58f14fedd19e
Andrzej Sterniewski <drapiszon@gmail.com>
2024-03-03 15:20:40 +0100

    add escaping special characters input

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -5,28 +5,37 @@ const Voter = require('../models/Voter.model');
 
 exports.add = async (req, res) => {
 
-  const validateEmail = (emailInput) => {
-    const regex = /^[a-zA-Z0–9._-]+@[a-zA-Z0–9.-]+\.[a-zA-Z]{2,4}$/;
+  const escape = (html) => {
+    return html
+      .replace(/&/g, '')
+      .replace(/</g, '')
+      .replace(/>/g, '')
+      .replace(/"/g, '')
+      .replace(/'/g, '');
+  };
 
-    if (emailInput.test(regex)) {
-      return true;
+  const validateEmail = (emailInput) => {
+    const regex = new RegExp(/^[a-zA-Z0–9._-]+@[a-zA-Z0–9.-]+\.[a-zA-Z]{2,4}$/);
+    const result = regex.test(emailInput);
+    if (result) {
+      return emailInput;
     }
     else {
-      throw new Error('Wrong input!');
-      // res.json('incorrect email');
-      // return false;
+      throw new Error('Wrong email!');
     }
-  }
+  };
 
   try {
     let { title, author, email } = req.fields;
-    email = validateEmail(email);
+    title = escape(title);
+    author = escape(author);
     const file = req.files.file;
 
     // task - validation title and author length
-    // if (title.length <= 25 && author.length <= 50) {}
+    if (title.length > 25 && author.length > 50) return res.status(400).json({ message: 'bad request' });
 
-    if (title && author && email && file) { // if fields are not empty...
+    if (title && author && email && file && validateEmail(email)) { // if fields are not empty...
+      // if (title && author && email && file) { // if fields are not empty...
 
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
       const fileExt = fileName.split('.').slice(-1)[0];
@@ -50,8 +59,8 @@ exports.add = async (req, res) => {
     }
   } catch (err) {
     res.status(500).json(err);
+    console.log(err);
   }
-
 };
 
 /****** LOAD ALL PHOTOS ********/
@@ -74,6 +83,19 @@ exports.vote = async (req, res) => {
     const photoToUpdate = await Photo.findOne({ _id: req.params.id });
     if (!photoToUpdate) res.status(404).json({ message: 'Not found' });
     else {
+
+      // tu sprawdzamy
+      // to daje adres ip klienta
+      req.clientIp;
+      const voter = await Voter.findOne({ user: req.clientIp });
+      if (!voter) {
+        const voter = new Voter({ user: req.clientIp, votes: [photoToUpdate._id] });
+        await voter.save();
+      } else {
+        // sprawdzamy czy voter głosował już na to zdjęcie,jeśli tak to return status
+        // a jeśli głosował to dopisujemy że głosował na nowe zdjecie
+
+      }
       photoToUpdate.votes++;
       photoToUpdate.save();
       res.send({ message: 'OK' });
--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const mongoose = require('mongoose');
 const formidable = require('express-formidable');
 const uniqid = require('uniqid');
 const connectToDB = require('./db');
+const requestIp = require('request-ip');
 
 // start express server
 const app = express();
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
 });
+
+app.use(requestIp.mw());
 
 // connect to DB
 connectToDB();
@@ -19,11 +22,11 @@ connectToDB();
 app.use(cors());
 app.use(formidable({ uploadDir: './public/uploads/' }, [{
   event: 'fileBegin', // on every file upload...
-    action: (req, res, next, name, file) => {
-      const fileName = uniqid() + '.' + file.name.split('.')[1];
-      file.path = __dirname + '/public/uploads/photo_' + fileName; // ...move the file to public/uploads with unique name
-    }
-  },
+  action: (req, res, next, name, file) => {
+    const fileName = uniqid() + '.' + file.name.split('.')[1];
+    file.path = __dirname + '/public/uploads/photo_' + fileName; // ...move the file to public/uploads with unique name
+  }
+},
 ]));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
